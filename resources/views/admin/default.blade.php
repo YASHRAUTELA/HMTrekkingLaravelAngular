1848e95c17533070fc42c952959edc54cdf16ecb
Ashwani Rautela <ashwani.rautela@gmail.com>
2018-06-13 17:17:26 +0530

    removed unrelevant menu items from admin dashboard

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -96,84 +96,24 @@
         <li class="active treeview">
           <a href="#">
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-envelope"></i>
+            <span>Messages</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="#"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Inbox</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Received</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Sent</a></li>
           </ul>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
-            <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Boxed</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-          </ul>
-        </li>
+
         
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="{{asset('admin/pages/calendar.html')}}">
-            <i class="fa fa-calendar"></i> <span>Calendar</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-red">3</small>
-              <small class="label pull-right bg-blue">17</small>
-            </span>
-          </a>
-        </li>
-        <li>
-          <a href="{{asset('admin/pages/mailbox/mailbox.html')}}">
-            <i class="fa fa-envelope"></i> <span>Mailbox</span>
-            <span class="pull-right-container">
-              <small class="label pull-right bg-yellow">12</small>
-              <small class="label pull-right bg-green">16</small>
-              <small class="label pull-right bg-red">5</small>
-            </span>
-          </a>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-folder"></i> <span>Examples</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{asset('admin/pages/examples/invoice.html')}}"><i class="fa fa-circle-o"></i> Invoice</a></li>
-            <li><a href="{{asset('admin/pages/examples/profile.html')}}"><i class="fa fa-circle-o"></i> Profile</a></li>
-            <li><a href="{{asset('admin/pages/examples/login.html')}}"><i class="fa fa-circle-o"></i> Login</a></li>
-            <li><a href="{{asset('admin/pages/examples/register.html')}}"><i class="fa fa-circle-o"></i> Register</a></li>
-            <li><a href="{{asset('admin/pages/examples/lockscreen.html')}}"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-            <li><a href="{{asset('admin/pages/examples/404.html')}}"><i class="fa fa-circle-o"></i> 404 Error</a></li>
-            <li><a href="{{asset('admin/pages/examples/500.html')}}"><i class="fa fa-circle-o"></i> 500 Error</a></li>
-            <li><a href="{{asset('admin/pages/examples/blank.html')}}"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-            <li><a href="{{asset('admin/pages/examples/pace.html')}}"><i class="fa fa-circle-o"></i> Pace Page</a></li>
-          </ul>
-        </li>
         <li class="treeview">
           <a href="#">
             <i class="fa fa-share"></i> <span>Multilevel</span>
@@ -207,7 +147,7 @@
             <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
           </ul>
         </li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+        
       </ul>
     </section>
     <!-- /.sidebar -->
@@ -345,14 +285,12 @@
 <script src="{{asset('admin/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="{{asset('admin/bower_components/jquery-ui/jquery-ui.min.js')}}"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 
 <!-- Bootstrap 3.3.7 -->
 <script src="{{asset('admin/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 
 <!-- AdminLTE App -->
 <script src="{{asset('admin/dist/js/adminlte.min.js')}}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 
 </body>
 </html>
